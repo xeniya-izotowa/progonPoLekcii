77f32e0a110b3a3039d8cd8b3806f17c362cd8a0
xeniya <xeniia.izotowa@gmail.com>
2021-08-09 23:05:00 +0300

    added shouldFindMaxWhenMaxIsntFirst

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -4,10 +4,11 @@ public class StatisticsService {
     public long findMax(long[] incomes) {
         long currentMaxIndex = 0;
         long currentMax = incomes[0];
-        for (long income : incomes)
+        for (long income : incomes) {
             if (currentMax < income) {
                 currentMax = income;
             }
+        }
         return currentMax;
     }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,11 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void shouldFindMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindMaxWhenMaxIsNotFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
 
